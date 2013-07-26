be416d092cac504a8ef912636eaa592fec0ec779
stephenamos <amos.stephen@gmail.com>
2013-07-26 16:18:50 -0700

    Wired up initial crime fragment to main crime activity class for outputting to the screen.

--- src/com/bignerdranch/android/criminalintent/CrimeActivity.java
+++ src/com/bignerdranch/android/criminalintent/CrimeActivity.java
@@ -1,8 +1,9 @@
 package com.bignerdranch.android.criminalintent;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
-import android.view.Menu;
+import android.support.v4.app.FragmentManager;
 
 public class CrimeActivity extends FragmentActivity {
 
@@ -10,6 +11,16 @@ public class CrimeActivity extends FragmentActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_crime);
+		
+		FragmentManager fm = getSupportFragmentManager();
+		Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+		
+		if (fragment == null) {
+			fragment = new CrimeFragment();
+			fm.beginTransaction()
+				.add(R.id.fragmentContainer, fragment)
+				.commit();
+		}
 	}
 
 

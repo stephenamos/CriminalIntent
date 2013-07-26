bbdf1af22915fbd363aaedb6280a1433ee5c0eeb
stephenamos <amos.stephen@gmail.com>
2013-07-26 15:31:50 -0700

    Added crime model object and corresponding fragment crime activity (And its corresponding fragment crime layout)

--- src/com/bignerdranch/android/criminalintent/CrimeActivity.java
+++ src/com/bignerdranch/android/criminalintent/CrimeActivity.java
@@ -1,10 +1,10 @@
 package com.bignerdranch.android.criminalintent;
 
 import android.os.Bundle;
-import android.app.Activity;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 
-public class CrimeActivity extends Activity {
+public class CrimeActivity extends FragmentActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -12,11 +12,5 @@ public class CrimeActivity extends Activity {
 		setContentView(R.layout.activity_crime);
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.crime, menu);
-		return true;
-	}
 
 }

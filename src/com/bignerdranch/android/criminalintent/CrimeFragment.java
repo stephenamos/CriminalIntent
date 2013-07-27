c2746c189c142cb242a7c3b53479df5f14ad1151
stephenamos <amos.stephen@gmail.com>
2013-07-27 11:10:16 -0700

    Allowed the list of crimes to open an additional activity corresponding to the crime clicked from the crime list.

--- src/com/bignerdranch/android/criminalintent/CrimeFragment.java
+++ src/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -1,5 +1,7 @@
 package com.bignerdranch.android.criminalintent;
 
+import java.util.UUID;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.text.Editable;
@@ -15,6 +17,8 @@ import android.widget.EditText;
 
 public class CrimeFragment extends Fragment {
 
+	public static final String EXTRA_CRIME_IDKEY = "com.bignerdranch.android.criminalintent.crime_id";
+	
 	private Crime mCrime;
 	private EditText mTitleField;
 	private Button mDateButton;
@@ -23,7 +27,8 @@ public class CrimeFragment extends Fragment {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		mCrime = new Crime();
+		UUID crimeId = (UUID) getActivity().getIntent().getSerializableExtra(EXTRA_CRIME_IDKEY);
+		mCrime = CrimeLab.getInstance(getActivity()).getCrime(crimeId);
 	}
 	
 	@Override
@@ -31,6 +36,7 @@ public class CrimeFragment extends Fragment {
 		View v = inflater.inflate(R.layout.fragment_crime, parent, false);
 		
 		mTitleField = (EditText) v.findViewById(R.id.crime_title);
+		mTitleField.setText(mCrime.getTitle());
 		mTitleField.addTextChangedListener(new TextWatcher() {
 			
 			@Override
@@ -56,6 +62,7 @@ public class CrimeFragment extends Fragment {
 		mDateButton.setEnabled(false);
 		
 		mSolvedCheckBox = (CheckBox) v.findViewById(R.id.crime_solved);
+		mSolvedCheckBox.setChecked(mCrime.isSolved());
 		mSolvedCheckBox.setOnCheckedChangeListener(new OnCheckedChangeListener() {
 
 			@Override
--- src/com/bignerdranch/android/criminalintent/CrimeListFragment.java
+++ src/com/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -2,6 +2,7 @@ package com.bignerdranch.android.criminalintent;
 
 import java.util.ArrayList;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.util.Log;
@@ -21,7 +22,7 @@ public class CrimeListFragment extends ListFragment {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		this.getActivity().setTitle(R.string.crimes_title);
+		getActivity().setTitle(R.string.crimes_title);
 		 //getActivity() is a convenience method provided by list fragment that returns the activity hosting this fragment
 
 		mCrimes = CrimeLab.getInstance(getActivity()).getCrimes();
@@ -33,7 +34,10 @@ public class CrimeListFragment extends ListFragment {
 	@Override
 	public void onListItemClick(ListView l, View v, int position, long id) {
 		Crime c = ((CrimeAdapter)getListAdapter()).getItem(position);
-		Log.d(TAG, c.getTitle() + " was clicked.");
+		Intent i = new Intent(getActivity(), CrimeActivity.class);
+		i.putExtra(CrimeFragment.EXTRA_CRIME_IDKEY, c.getId());
+		
+		startActivity(i);
 	}
 	
 	private class CrimeAdapter extends ArrayAdapter<Crime> {

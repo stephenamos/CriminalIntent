da7c2bfcfcfe939b383c5b7c65a0c4b138209893
stephenamos <amos.stephen@gmail.com>
2013-07-28 15:20:47 -0700

    Added a date picker to the alert dialog fragment

--- gen/com/bignerdranch/android/criminalintent/R.java
+++ gen/com/bignerdranch/android/criminalintent/R.java
@@ -35,8 +35,9 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_fragment=0x7f030000;
-        public static final int fragment_crime=0x7f030001;
-        public static final int list_item_crime=0x7f030002;
+        public static final int dialog_date=0x7f030001;
+        public static final int fragment_crime=0x7f030002;
+        public static final int list_item_crime=0x7f030003;
     }
     public static final class string {
         public static final int app_name=0x7f060000;
--- src/com/bignerdranch/android/criminalintent/DatePickerFragment.java
+++ src/com/bignerdranch/android/criminalintent/DatePickerFragment.java
@@ -4,12 +4,16 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
+import android.view.View;
 
 public class DatePickerFragment extends DialogFragment {
 	
 	@Override
 	public Dialog onCreateDialog(Bundle savedInstanceState) {
+		View view = getActivity().getLayoutInflater().inflate(R.layout.dialog_date, null);
+		
 		return new AlertDialog.Builder(getActivity())
+			.setView(view)
 			.setTitle(R.string.date_picker_title)
 			.setPositiveButton(android.R.string.ok, null)
 			.create();
